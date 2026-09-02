d39c0825a59cd3a9f61de840a90ada2deab13710
RenatoGRS <reluhegeresa@gmail.com>
2026-09-02 15:22:45 -0500

    futbol femenino, segunda fecha

--- data/futbol-femenino.js
+++ data/futbol-femenino.js
@@ -22,6 +22,29 @@ const futbolFemMatches = [
 	},
 
 
+	{
+		fecha:"02/09/2026", hora:"13:10",
+		filas:[
+			{ equipo:"Salud Integral",  		goles:1 },
+			{ equipo:"Saneamiento ambiental", 	goles:0 }
+		]
+	},
+	{
+		fecha:"02/09/2026", hora:"2:00",
+		filas:[
+			{ equipo:"Inteligencia sanitaria",  goles:1 },
+			{ equipo:"Administración", 			goles:1 }
+		]
+	},
+	{
+		fecha:"02/09/2026", hora:"2:00",
+		filas:[
+			{ equipo:"Calidad sanitaria",  		goles:1 },
+			{ equipo:"Planificación y RRHH", 	goles:1 }
+		]
+	},
+
+
 
 ];
 
